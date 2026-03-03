72a9e72bbe704fe1d5c6abdc59bc32fb579cae2a
20251ti0075-eng <20251ti0075@ifba.edu.br>
2026-03-03 11:35:31 -0300

    Update TV.java

--- TV.java
+++ TV.java
@@ -1,1 +1,15 @@
-
+class TV{
+  string Marca;
+  string Modelo;
+  int Canal;
+  
+  void ligar(){
+    //ligar televisão
+  }
+  void desligar(){
+    //desligar televisão
+  }
+  void mudar_canal(){
+    //mudar o canal
+  }
+}
